f36a1dbf15e1eb1f8caf90c1115af6749204fda3
Alessandra-Sousa <alessandrasousadacruz@gmail.com>
2023-01-02 21:08:16 -0300

    implementa request e response

--- src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteRequest.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteRequest.java
@@ -1,5 +1,34 @@
 package br.com.petz.clientepet.cliente.application.api;
 
-public class ClienteRequest {
+import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.br.CPF;
+
+import br.com.petz.clientepet.cliente.domain.Sexo;
+import lombok.Value;
+
+@Value
+public class ClienteRequest {
+	@NotBlank
+	private String nomeCompleto;
+	@NotBlank
+	@Email
+	@Column(unique = true)
+	private String email;
+	@NotBlank
+	private String celular;
+	private String telefone;
+	private Sexo sexo;
+	@NotNull
+	private LocalDate dataNascimento;
+	@CPF
+	@Column(unique = true)
+	private String cpf;
+	@NotNull
+	private Boolean aceitaTermos;
 }
