bfcdd5dbd57ea49391b890f4283070f5309b2171
Alessandra-Sousa <alessandrasousadacruz@gmail.com>
2023-01-05 20:58:17 -0300

    implementa fluxo de servico de aplicacao

--- src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteAPI.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteAPI.java
@@ -14,8 +14,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import lombok.Value;
-
 @RestController
 @RequestMapping("/v1/cliente")
 public interface ClienteAPI {
