446cc5cebe2c47c69cac58c9afedcfb9038fd56d
Alessandra-Sousa <alessandrasousadacruz@gmail.com>
2023-03-08 11:26:48 -0300

    implementa fluxo petService buscaPet detalhado

--- src/main/java/br/com/petz/clientepet/pet/application/service/PetApplicationService.java
+++ src/main/java/br/com/petz/clientepet/pet/application/service/PetApplicationService.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 import org.springframework.stereotype.Service;
 
 import br.com.petz.clientepet.cliente.application.service.ClienteService;
+import br.com.petz.clientepet.pet.application.api.PetClienteDetalheResponse;
 import br.com.petz.clientepet.pet.application.api.PetClienteListResponse;
 import br.com.petz.clientepet.pet.application.api.PetRequest;
 import br.com.petz.clientepet.pet.application.api.PetResponse;
@@ -38,6 +39,13 @@ public class PetApplicationService implements PetService {
 		List<Pet> petsDoCliente = petRepository.buscaPetsDoClienteComId(idCliente);
 		log.info("[finaliza] PetApplicationService - buscaPetsDoClienteComId");
 		return PetClienteListResponse.converte(petsDoCliente);
+	}
+
+	@Override
+	public PetClienteDetalheResponse buscaPetDoClienteComID(UUID idCliente, UUID idPet) {
+		log.info("[inicia] PetApplicationService - buscaPetDoClienteComID");
+		log.info("[finaliza] PetApplicationService - buscaPetDoClienteComID");
+		return null;
 	}
 
 }
